d3ad2dbf8db63cce690ba9dde382b07452fa3bc1
Mohamadrjk <hvnclsh@gmail.com>
2024-01-10 18:23:58 +0330

    password show hide action

--- src/components/Login/InputArea.js
+++ src/components/Login/InputArea.js
@@ -1,30 +1,45 @@
 "use client";
-import React, { useRef } from "react";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
 
 function InputArea(props) {
-  const currentInput = useRef(null);
+  // change event handler
   const handleChange = (event) => {
     event.preventDefault();
     props.onChange(event.target.value);
   };
 
+  // password  show/hide toggler
+  const [passwordType, setPaswordType] = useState("password");
   const handleShowPassword = () => {
-    console.log(currentInput.current?.type);
+    passwordType == "password"
+      ? setPaswordType("text")
+      : setPaswordType("password");
   };
   return (
     <div className=" flex flex-col text-white md:text-black gap-2">
       <label htmlFor={props.name}>{props.title}</label>
       <div className=" relative w-full flex ">
         <input
-          type={props.type}
+          type={props.type != "password" ? props.type : passwordType}
           id={props.name}
-          ref={currentInput}
           className={`${props.className} pb-2  min-w-full mb-5 md:max-w-80  md:min-w-56  border-b md:placeholder:text-black placeholder:text-white bg-transparent  border-[#d0d1d2] focus-visible:ring-0 focus-visible:outline-none focus:ring-0`}
           disabled={props.disabled}
           onChange={handleChange}
           placeholder={props.placeholder}
         />
-        {props.type == "password" && <span onClick={handleShowPassword}></span>}
+        {props.type == "password" && (
+          <span
+            onClick={handleShowPassword}
+            className="absolute right-0 top-0 cursor-pointer "
+          >
+            {passwordType == "text" ? (
+              <Image width={25} height={25} src={"/assets/hide.png"} />
+            ) : (
+              <Image width={25} height={25} src={"/assets/show.png"} />
+            )}
+          </span>
+        )}
       </div>
     </div>
   );
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -8,21 +8,38 @@ function LoginForm() {
   const [EmailInput, setEmailInput] = useState("");
   const [passWordInput, setPassWordInput] = useState("");
   const [isDisabled, setisDisabled] = useState(true);
-
+  const [EmailError, setEmailError] = useState(true);
+  const [PasswordError, setPasswordError] = useState(true);
+  const EmailRegex = new RegExp(
+    "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
+  );
+  const PasswordRegex = new RegExp("^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$");
   //   handlers
   const EmailHandler = (value) => {
     setEmailInput(value);
+    if (!EmailRegex.test(EmailInput) || EmailInput.length === 0) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+    console.log(EmailRegex.test(EmailInput));
   };
   const PasswordHandler = (value) => {
     setPassWordInput(value);
+    if (PasswordRegex.test(passWordInput) || passWordInput.length == 0) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
   };
   useEffect(() => {
-    if (passWordInput.length == 0 || EmailInput.length == 0) {
+    console.log(PasswordError, EmailError);
+    if (PasswordError == true || EmailError == true) {
       setisDisabled(true);
     } else {
       setisDisabled(false);
     }
-  }, [passWordInput, EmailInput]);
+  }, [PasswordError, EmailError]);
   return (
     <form className="flex flex-col">
       <div className="flex flex-col mb-5">
@@ -31,14 +48,22 @@ function LoginForm() {
           name="Email_input"
           onChange={EmailHandler}
           title="Email"
+          type="text"
           placeHolder=""
         />
+        {EmailError && (
+          <span className=" text-red-600">*please enter email address</span>
+        )}
         <InputArea
           onChange={PasswordHandler}
           name="Pasword_input"
           title="Password"
+          type="password"
           placeHolder=""
         />
+        {PasswordError && (
+          <span className=" text-red-600">*please enter password</span>
+        )}
       </div>
       <Button title="Sign in" disabled={isDisabled} />
     </form>
